c16641a337590940aba191f74d5bee2635352e75
Dan Katzen <37639581+dankatzen@users.noreply.github.com>
2021-11-14 18:34:38 -0800

    first successful build

--- src/fibonacci-demoreelDK.cpp
+++ src/fibonacci-demoreelDK.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 /*
    Fibonacci Demo Reel: https://github.com/jasoncoon/fibonacci-demoreel
    Copyright (C) 2020 Jason Coon
@@ -33,6 +35,9 @@ FASTLED_USING_NAMESPACE
 #define MILLI_AMPS         2000 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND  120  // here you can control the speed. With the Access Point / Web Server the animations run a bit slower.
 
+
+
+
 CRGB leds[NUM_LEDS];
 
 const uint8_t brightnessCount = 5;
@@ -63,6 +68,8 @@ uint8_t cooling = 49;
 uint8_t sparking = 60;
 
 uint8_t speed = 30;
+
+
 
 ///////////////////////////////////////////////////////////////////////
 
@@ -126,213 +133,65 @@ typedef Pattern PatternList[];
 #include "Twinkles.h"
 #include "TwinkleFOX.h"
 #include "Map.h"
-//#include "Noise.h"
 
-// List of patterns to cycle through.  Each is defined as a separate function below.
-
-PatternList patterns = {
-//  pride,
-  prideFibonacci,
-//  colorWaves,
-  colorWavesFibonacci,
-
-  // matrix patterns
-  anglePalette,
-  radiusPalette,
-  xPalette,
-  yPalette,
-  xyPalette,
-
-  angleGradientPalette,
-  radiusGradientPalette,
-  xGradientPalette,
-  yGradientPalette,
-  xyGradientPalette,
-
-//  // noise patterns
-//  fireNoise,
-//  fireNoise2,
-//  lavaNoise,
-//  rainbowNoise,
-//  rainbowStripeNoise,
-//  partyNoise,
-//  forestNoise,
-//  cloudNoise,
-//  oceanNoise,
-//  blackAndWhiteNoise,
-//  blackAndBlueNoise,
-
-  // twinkle patterns
-  rainbowTwinkles,
-  snowTwinkles,
-  cloudTwinkles,
-  incandescentTwinkles,
-
-  // TwinkleFOX patterns
-  retroC9Twinkles,
-  redWhiteTwinkles,
-  blueWhiteTwinkles,
-  redGreenWhiteTwinkles,
-  fairyLightTwinkles,
-  snow2Twinkles,
-  hollyTwinkles,
-  iceTwinkles,
-  partyTwinkles,
-  forestTwinkles,
-  lavaTwinkles,
-  fireTwinkles,
-  cloud2Twinkles,
-  oceanTwinkles,
-
-  rainbow,
-  rainbowWithGlitter,
-  rainbowSolid,
-  confetti,
-  sinelon,
-  bpm,
-  juggle,
-  fire,
-  water,
-
-//  strandTest,
-//
-//  showSolidColor
-};
-
-const uint8_t patternCount = ARRAY_SIZE(patterns);
-
-void handleInput() {
-  if (buttonPattern.released())
-  {
-    Serial.println("Pattern button released");
-    if (autoplay) {
-      autoplay = 0;
-      Serial.println("Autoplay: off");
-    }
-    adjustPattern(true);
-  }
-
-  if (buttonBrightness.released()) {
-    Serial.println("Brightness button released");
-    adjustBrightness(true);
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  delay(100);
-
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  FastLED.setDither(false);
-  FastLED.setCorrection(TypicalSMD5050);
-  FastLED.setBrightness(brightness);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
-
-  FastLED.setBrightness(brightness);
-
-  autoPlayTimeout = millis() + (autoplayDuration * 1000);
-
-  buttonBrightness.begin();
-  buttonPattern.begin();
-}
-
-void loop() {
-  // Add entropy to random number generator; we use a lot of it.
-  random16_add_entropy(random(65535));
-
-  handleInput();
-
-  if (power == 0) {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    FastLED.show();
-    delay(1000 / FRAMES_PER_SECOND);
-    return;
-  }
-
-  // change to a new cpt-city gradient palette
-  EVERY_N_SECONDS( secondsPerPalette ) {
-    gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
-    gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
-  }
-
-  EVERY_N_MILLISECONDS(40) {
-    // slowly blend the current palette to the next
-    nblendPaletteTowardPalette( gCurrentPalette, gTargetPalette, 8);
-    gHue++;  // slowly cycle the "base color" through the rainbow
-  }
-
-  if (autoplay && (millis() > autoPlayTimeout)) {
-    adjustPattern(true);
-    autoPlayTimeout = millis() + (autoplayDuration * 1000);
-  }
-
-  // Call the current pattern function once, updating the 'leds' array
-  patterns[currentPatternIndex]();
-
-  FastLED.show();
-
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000 / FRAMES_PER_SECOND);
-}
-
-// increase or decrease the current pattern number, and wrap around at the ends
-void adjustPattern(bool up)
-{
-  if (up)
-    currentPatternIndex++;
-  else
-    currentPatternIndex--;
-
-  // wrap around at the ends
-  if (currentPatternIndex < 0)
-    currentPatternIndex = patternCount - 1;
-  if (currentPatternIndex >= patternCount)
-    currentPatternIndex = 0;
-
-  Serial.print("pattern: ");
-  Serial.println(currentPatternIndex);
-}
-
-// increase or decrease the current brightness, and wrap around at the ends
-void adjustBrightness(bool up)
-{
-  if (up)
-    brightnessIndex++;
-  else
-    brightnessIndex--;
-
-  // wrap around at the ends
-  if (brightnessIndex < 0)
-    brightnessIndex = brightnessCount - 1;
-  if (brightnessIndex >= brightnessCount)
-    brightnessIndex = 0;
-
-  brightness = brightnessMap[brightnessIndex];
-  FastLED.setBrightness(brightness);
-
-  Serial.print("brightness: ");
-  Serial.println(brightness);
-}
-
-void strandTest()
-{
-  uint8_t i = speed;
-
-  if (i >= NUM_LEDS) i = NUM_LEDS - 1;
-
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-
-  leds[i] = solidColor;
-}
-
-void showSolidColor()
-{
-  fill_solid(leds, NUM_LEDS, solidColor);
-}
+//FORWARD DECs
 
 // Patterns from FastLED example DemoReel100: https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
+
+void addGlitter( uint8_t chanceOfGlitter)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+
+// based on FastLED example Fire2012WithPalette: https://github.com/FastLED/FastLED/blob/master/examples/Fire2012WithPalette/Fire2012WithPalette.ino
+void heatMap(CRGBPalette16 palette, bool up)
+{
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+
+  // Add entropy to random number generator; we use a lot of it.
+  random16_add_entropy(random(256));
+
+  // Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  byte colorindex;
+
+  // Step 1.  Cool down every cell a little
+  for ( uint16_t i = 0; i < NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((cooling * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for ( uint16_t k = NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if ( random8() < sparking ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160, 255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for ( uint16_t j = 0; j < NUM_LEDS; j++) {
+    // Scale the heat value from 0-255 down to 0-240
+    // for best results with color palettes.
+    colorindex = scale8(heat[j], 190);
+
+    CRGB color = ColorFromPalette(palette, colorindex);
+
+    if (up) {
+      leds[j] = color;
+    }
+    else {
+      leds[(NUM_LEDS - 1) - j] = color;
+    }
+  }
+}
+
 
 void rainbow()
 {
@@ -433,13 +292,8 @@ void water()
 // Pride2015 by Mark Kriegsman: https://gist.github.com/kriegsman/964de772d64c502760e5
 // This function draws rainbows with an ever-changing,
 // widely-varying set of parameters.
-void pride() {
-  fillWithPride(false);
-}
 
-void prideFibonacci() {
-  fillWithPride(true);
-}
+
 
 void fillWithPride(bool useFibonacciOrder) {
   static uint16_t sPseudotime = 0;
@@ -484,100 +338,14 @@ void fillWithPride(bool useFibonacciOrder) {
   }
 }
 
-void radialPaletteShift()
-{
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    // leds[i] = ColorFromPalette( gCurrentPalette, gHue + sin8(i*16), brightness);
-    leds[fibonacciToPhysical[i]] = ColorFromPalette(gCurrentPalette, i + gHue, 255, LINEARBLEND);
-  }
+void pride() {
+  fillWithPride(false);
 }
 
-void radialPaletteShiftOutward()
-{
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    leds[fibonacciToPhysical[i]] = ColorFromPalette(gCurrentPalette, i - gHue, 255, LINEARBLEND);
-  }
+void prideFibonacci() {
+  fillWithPride(true);
 }
 
-// based on FastLED example Fire2012WithPalette: https://github.com/FastLED/FastLED/blob/master/examples/Fire2012WithPalette/Fire2012WithPalette.ino
-void heatMap(CRGBPalette16 palette, bool up)
-{
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-
-  // Add entropy to random number generator; we use a lot of it.
-  random16_add_entropy(random(256));
-
-  // Array of temperature readings at each simulation cell
-  static byte heat[NUM_LEDS];
-
-  byte colorindex;
-
-  // Step 1.  Cool down every cell a little
-  for ( uint16_t i = 0; i < NUM_LEDS; i++) {
-    heat[i] = qsub8( heat[i],  random8(0, ((cooling * 10) / NUM_LEDS) + 2));
-  }
-
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for ( uint16_t k = NUM_LEDS - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-  }
-
-  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-  if ( random8() < sparking ) {
-    int y = random8(7);
-    heat[y] = qadd8( heat[y], random8(160, 255) );
-  }
-
-  // Step 4.  Map from heat cells to LED colors
-  for ( uint16_t j = 0; j < NUM_LEDS; j++) {
-    // Scale the heat value from 0-255 down to 0-240
-    // for best results with color palettes.
-    colorindex = scale8(heat[j], 190);
-
-    CRGB color = ColorFromPalette(palette, colorindex);
-
-    if (up) {
-      leds[j] = color;
-    }
-    else {
-      leds[(NUM_LEDS - 1) - j] = color;
-    }
-  }
-}
-
-void addGlitter( uint8_t chanceOfGlitter)
-{
-  if ( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
-  }
-}
-
-///////////////////////////////////////////////////////////////////////
-
-// Forward declarations of an array of cpt-city gradient palettes, and
-// a count of how many there are.  The actual color palette definitions
-// are at the bottom of this file.
-extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
-extern const uint8_t gGradientPaletteCount;
-
-uint8_t beatsaw8( accum88 beats_per_minute, uint8_t lowest = 0, uint8_t highest = 255,
-                  uint32_t timebase = 0, uint8_t phase_offset = 0)
-{
-  uint8_t beat = beat8( beats_per_minute, timebase);
-  uint8_t beatsaw = beat + phase_offset;
-  uint8_t rangewidth = highest - lowest;
-  uint8_t scaledbeat = scale8( beatsaw, rangewidth);
-  uint8_t result = lowest + scaledbeat;
-  return result;
-}
-
-void colorWaves() {
-  fillWithColorWaves(leds, NUM_LEDS, gCurrentPalette, false);
-}
-
-void colorWavesFibonacci() {
-  fillWithColorWaves(leds, NUM_LEDS, gCurrentPalette, true);
-}
 
 // ColorWavesWithPalettes by Mark Kriegsman: https://gist.github.com/kriegsman/8281905786e8b2632aeb
 // This function draws color waves with an ever-changing,
@@ -644,3 +412,261 @@ void palettetest( CRGB* ledarray, uint16_t numleds, const CRGBPalette16& gCurren
   startindex--;
   fill_palette( ledarray, numleds, startindex, (256 / NUM_LEDS) + 1, gCurrentPalette, 255, LINEARBLEND);
 }
+
+
+void colorWaves() {
+  fillWithColorWaves(leds, NUM_LEDS, gCurrentPalette, false);
+}
+
+void colorWavesFibonacci() {
+  fillWithColorWaves(leds, NUM_LEDS, gCurrentPalette, true);
+}
+
+
+// increase or decrease the current brightness, and wrap around at the ends
+void adjustBrightness(bool up)
+{
+  if (up)
+    brightnessIndex++;
+  else
+    brightnessIndex--;
+
+  // wrap around at the ends
+  if (brightnessIndex < 0)
+    brightnessIndex = brightnessCount - 1;
+  if (brightnessIndex >= brightnessCount)
+    brightnessIndex = 0;
+
+  brightness = brightnessMap[brightnessIndex];
+  FastLED.setBrightness(brightness);
+
+  Serial.print("brightness: ");
+  Serial.println(brightness);
+}
+
+
+
+
+//#include "Noise.h"
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+
+PatternList patterns = {
+//  pride,
+  prideFibonacci,
+//  colorWaves,
+  colorWavesFibonacci,
+
+  // matrix patterns
+  anglePalette,
+  radiusPalette,
+  xPalette,
+  yPalette,
+  xyPalette,
+
+  angleGradientPalette,
+  radiusGradientPalette,
+  xGradientPalette,
+  yGradientPalette,
+  xyGradientPalette,
+
+//  // noise patterns
+//  fireNoise,
+//  fireNoise2,
+//  lavaNoise,
+//  rainbowNoise,
+//  rainbowStripeNoise,
+//  partyNoise,
+//  forestNoise,
+//  cloudNoise,
+//  oceanNoise,
+//  blackAndWhiteNoise,
+//  blackAndBlueNoise,
+
+  // twinkle patterns
+  rainbowTwinkles,
+  snowTwinkles,
+  cloudTwinkles,
+  incandescentTwinkles,
+
+  // TwinkleFOX patterns
+  retroC9Twinkles,
+  redWhiteTwinkles,
+  blueWhiteTwinkles,
+  redGreenWhiteTwinkles,
+  fairyLightTwinkles,
+  snow2Twinkles,
+  hollyTwinkles,
+  iceTwinkles,
+  partyTwinkles,
+  forestTwinkles,
+  lavaTwinkles,
+  fireTwinkles,
+  cloud2Twinkles,
+  oceanTwinkles,
+  rainbow,
+  rainbowWithGlitter,
+  rainbowSolid,
+  confetti,
+  sinelon,
+  bpm,
+  juggle,
+  fire,
+  water,
+
+//  strandTest,
+//
+//  showSolidColor
+};
+
+const uint8_t patternCount = ARRAY_SIZE(patterns);
+
+// increase or decrease the current pattern number, and wrap around at the ends
+void adjustPattern(bool up)
+{
+  if (up)
+    currentPatternIndex++;
+  else
+    currentPatternIndex--;
+
+  // wrap around at the ends
+  if (currentPatternIndex < 0)
+    currentPatternIndex = patternCount - 1;
+  if (currentPatternIndex >= patternCount)
+    currentPatternIndex = 0;
+
+  Serial.print("pattern: ");
+  Serial.println(currentPatternIndex);
+}
+
+
+void handleInput() {
+  if (buttonPattern.released())
+  {
+    Serial.println("Pattern button released");
+    if (autoplay) {
+      autoplay = 0;
+      Serial.println("Autoplay: off");
+    }
+    adjustPattern(true);
+  }
+
+  if (buttonBrightness.released()) {
+    Serial.println("Brightness button released");
+    adjustBrightness(true);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setDither(false);
+  FastLED.setCorrection(TypicalSMD5050);
+  FastLED.setBrightness(brightness);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+
+  FastLED.setBrightness(brightness);
+
+  autoPlayTimeout = millis() + (autoplayDuration * 1000);
+
+  buttonBrightness.begin();
+  buttonPattern.begin();
+}
+
+void loop() {
+  // Add entropy to random number generator; we use a lot of it.
+  random16_add_entropy(random(65535));
+
+  handleInput();
+
+  if (power == 0) {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(1000 / FRAMES_PER_SECOND);
+    return;
+  }
+
+  // change to a new cpt-city gradient palette
+  EVERY_N_SECONDS( secondsPerPalette ) {
+    gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
+    gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
+  }
+
+  EVERY_N_MILLISECONDS(40) {
+    // slowly blend the current palette to the next
+    nblendPaletteTowardPalette( gCurrentPalette, gTargetPalette, 8);
+    gHue++;  // slowly cycle the "base color" through the rainbow
+  }
+
+  if (autoplay && (millis() > autoPlayTimeout)) {
+    adjustPattern(true);
+    autoPlayTimeout = millis() + (autoplayDuration * 1000);
+  }
+
+  // Call the current pattern function once, updating the 'leds' array
+  patterns[currentPatternIndex]();
+
+  FastLED.show();
+
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
+}
+
+
+void strandTest()
+{
+  uint8_t i = speed;
+
+  if (i >= NUM_LEDS) i = NUM_LEDS - 1;
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+
+  leds[i] = solidColor;
+}
+
+void showSolidColor()
+{
+  fill_solid(leds, NUM_LEDS, solidColor);
+}
+
+
+void radialPaletteShift()
+{
+  for (uint16_t i = 0; i < NUM_LEDS; i++) {
+    // leds[i] = ColorFromPalette( gCurrentPalette, gHue + sin8(i*16), brightness);
+    leds[fibonacciToPhysical[i]] = ColorFromPalette(gCurrentPalette, i + gHue, 255, LINEARBLEND);
+  }
+}
+
+void radialPaletteShiftOutward()
+{
+  for (uint16_t i = 0; i < NUM_LEDS; i++) {
+    leds[fibonacciToPhysical[i]] = ColorFromPalette(gCurrentPalette, i - gHue, 255, LINEARBLEND);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////
+
+// Forward declarations of an array of cpt-city gradient palettes, and
+// a count of how many there are.  The actual color palette definitions
+// are at the bottom of this file.
+extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
+extern const uint8_t gGradientPaletteCount;
+
+uint8_t beatsaw8( accum88 beats_per_minute, uint8_t lowest = 0, uint8_t highest = 255,
+                  uint32_t timebase = 0, uint8_t phase_offset = 0)
+{
+  uint8_t beat = beat8( beats_per_minute, timebase);
+  uint8_t beatsaw = beat + phase_offset;
+  uint8_t rangewidth = highest - lowest;
+  uint8_t scaledbeat = scale8( beatsaw, rangewidth);
+  uint8_t result = lowest + scaledbeat;
+  return result;
+}
+
+
+
